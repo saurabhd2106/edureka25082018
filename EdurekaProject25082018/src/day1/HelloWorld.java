9ba02e97567ca93fc2aaa1e9b6b50dd62f871a0e
Saurabh Dhingra <saurabh.d2106@gmail.com>
2018-08-25 23:20:53 +0530

    Updated hello world

--- EdurekaProject25082018/src/day1/HelloWorld.java
+++ EdurekaProject25082018/src/day1/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		System.out.println();
+		System.out.println("Hello world again");
 	}
 
 }
